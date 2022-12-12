4b8237a7279b1a6fc09aa6ff40553a94e49f4590
Kaleb Garrett <kalebagarrett@gmail.com>
2022-12-12 16:07:37 -0500

    Added login feature.

--- LoginAttempt/Credentials.cs
+++ LoginAttempt/Credentials.cs
@@ -1,7 +1,55 @@
-﻿namespace LoginAttempt
+﻿using System;
+
+namespace LoginAttempt
 {
     public class Credentials
     {
+        public int attempt = 0;
+        public string userName = "ABC";
+        public string passWord = "123";
+        public string loginUsername;
+        public string loginPassword;
         
+        public void loginCredentials()
+        {
+            Console.Write("Enter your username: ");
+            loginUsername = Console.ReadLine();
+            Console.Write("Enter your password: ");
+            loginPassword = Console.ReadLine();
+            
+            if (loginUsername != userName)
+            {
+                Console.WriteLine("You have submitted an incorrect username! Try again.");
+            }
+            
+            if (loginPassword != passWord)
+            {
+                Console.WriteLine("You have submitted an incorrect password! Try again.");
+            }
+        }
+
+        public void loginCounter()
+        {
+            if (loginUsername != userName || loginPassword != passWord)
+            {
+                attempt++;
+            }
+            else
+            {
+                attempt = 1;
+            }
+        }
+        
+        public void attemptMax()
+        {
+            if (attempt == 3)
+            {
+                Console.WriteLine("Login attempt reached three times. Try again later!");
+            }
+            else
+            {
+                Console.WriteLine("Login Successful!");
+            }
+        }
     }
 }
--- LoginAttempt/Program.cs
+++ LoginAttempt/Program.cs
@@ -4,9 +4,19 @@ namespace LoginAttempt
 {
     class Program
     {
+        //The following program allows you to input a username and password based
+        //off the variables userName and passWord under the Credentials class.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Credentials credentials = new Credentials();
+
+            do
+            {
+                credentials.loginCredentials();
+                credentials.loginCounter();
+            } while ((credentials.loginUsername != credentials.userName || credentials.loginPassword != credentials.passWord) && credentials.attempt != 3);
+            
+            credentials.attemptMax();
         }
     }
 }
